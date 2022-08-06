a87b4f06cf771bcced10264db1e301de02aa36f6
Steven <stevenabowman1@gmail.com>
2022-08-06 12:15:49 -0400

    Dragon Battle Game Week 1 Workshop

--- battlegame.py
+++ battlegame.py
@@ -1,33 +1,56 @@
-"""
-battlegame.py
-Dragon Battle Roleplay
-"""
+wizard = "Wizard"
+elf = "Elf"
+human = "Human"
 
+wizard_hp = 70
+elf_hp = 100
+human_hp = 150
 
-def main():
-    # define variables
-    # ask for user input, enumerate list, provide list for user and change input from str to int
-    wizard = "Wizard"
-    elf = "Elf"
-    human = "Human"
+wizard_damage = 150
+elf_damage = 100
+human_damage = 20
 
-    wizard_hp = 70
-    elf_hp = 100
-    human_hp = 150
+dragon_hp = 300
+dragon_damage = 50
 
-    wizard_damage = 150
-    elf_damage = 100
-    human_damage = 20
+while True:
+    print("1) Wizard")
+    print("2) Elf")
+    print("3) Human")
+    character = input("Choose Your Character:")
 
-    dragon_hp = 300
-    dragon_damage = 50
+    if character == "1" or character.lower() == "Wizard":
+        character = wizard
+        my_hp = wizard_hp
+        my_damage = wizard_damage
+        break
+    if character == "2" or character.lower() == "Elf":
+        character = elf
+        my_hp = elf_hp
+        my_damage = elf_damage
+        break
+    if character == "3" or character.lower() == "Human":
+        character = human
+        my_hp = human_hp
+        my_damage = human_damage
+        break
+    print("Unknown Character")
+print("Character:", character)
+print("Health:", my_hp)
+print("Damage:", my_damage, "\n")
 
-    character_list = [wizard, elf, human]
+while True:
+    dragon_hp = dragon_hp - my_damage
+    print("The", character, "damaged the Dragon!")
 
-    for i, value in enumerate(character_list, 1):
-        print("{}.) {}".format(i, value))
+    if dragon_hp <= 0:
+        print("The Dragon has lost the battle", dragon_hp)
+        break
 
-    character = (int(input("Choose Your Character:")))
+    my_hp = my_hp - dragon_damage
+    print("The Dragon damaged the", character)
+    print("Your hitpoints are", my_hp)
 
-
-main()
+    if my_hp <= 0:
+        print("The", character, "has lost the battle!")
+        break
